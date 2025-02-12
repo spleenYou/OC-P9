fc8f77153ba76ebd7552015fd7e22d25c5cdd820
Anthony YOU <spleen85@proton.me>
2025-02-12 15:20:34 +0100

    add home page

--- LITRevu/settings.py
+++ LITRevu/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
     'book'
 ]
 
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'LITRevu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR.joinpath('templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authentication.User'
--- LITRevu/urls.py
+++ LITRevu/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import book.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', book.views.home, name='home')
 ]
