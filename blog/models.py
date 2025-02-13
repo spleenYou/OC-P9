36e909f4aa6c44e9841a31bd3eb7f78dc2463caa
Anthony YOU <spleen85@proton.me>
2025-02-13 17:10:15 +0100

    add models on blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(
+        max_length=2048,
+        blank=True
+    )
+    image = models.ImageField(null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+    ticket = models.ForeignKey("Ticket", on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)]
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=128)
+    body = models.TextField(
+        max_length=8192,
+        blank=True
+    )
+    time_created = models.DateTimeField(auto_now_add=False)
+
+
+class UserFollows(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='following',
+    )
+    followed_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='followed_by'
+    )
+
+    class Meta:
+        unique_together = ('user', 'followed_user')
